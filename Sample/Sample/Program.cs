9c17d79414713904cb60df3edcfde1ca5188bd94
sidheswar_ghosh@infosys.com <sidheswar_ghosh@infosys.com>
2015-10-08 09:01:35 +0530

    commit

---
+++ Sample/Sample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            int a = 50;
+            int b = 40;
+            int sum = a + b;
+            return sum;
+        }
+    }
+}
